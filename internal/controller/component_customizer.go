dc4bd7d4fd2d62fa03d581882291dd034a08b6ef
Danil Grigorev <danil.grigorev@suse.com>
2023-12-11 14:01:03 +0100

    Fix applied manifests ownership reference set
    - Prevent applied manifes from duplicating owner references from the
      provider object.

--- internal/controller/component_customizer.go
+++ internal/controller/component_customizer.go
@@ -58,7 +58,12 @@ func customizeObjectsFn(provider genericprovider.GenericProvider) func(objs []un
 
 			if o.GetNamespace() != "" {
 				// only set the ownership on namespaced objects.
-				o.SetOwnerReferences(util.EnsureOwnerRef(provider.GetOwnerReferences(),
+				ownerReferences := o.GetOwnerReferences()
+				if ownerReferences == nil {
+					ownerReferences = []metav1.OwnerReference{}
+				}
+
+				o.SetOwnerReferences(util.EnsureOwnerRef(ownerReferences,
 					metav1.OwnerReference{
 						APIVersion: operatorv1.GroupVersion.String(),
 						Kind:       provider.GetObjectKind().GroupVersionKind().Kind,
