ec7a7aa6ba058d086b91d5f23739b9d71714fc34
alexander-demicev <alexandr.demicev@suse.com>
2024-04-09 12:37:53 +0200

    Use absolute path in update scripts
    Signed-off-by: alexander-demicev <alexandr.demicev@suse.com>

--- hack/update-plugin-yaml.sh
+++ hack/update-plugin-yaml.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o pipefail
 
-REPO_ROOT=$(dirname "${BASH_SOURCE}")/..
+# Resolve the absolute path of the directory containing the script
+SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+REPO_ROOT="$SCRIPT_DIR/.."
 
-docker run --rm -v $REPO_ROOT:/home/app ghcr.io/rajatjindal/krew-release-bot:v0.0.46 krew-release-bot template --tag $1 --template-file .krew.yaml > $REPO_ROOT/plugins/clusterctl-operator.yaml
+docker run --rm -v "$REPO_ROOT":/home/app ghcr.io/rajatjindal/krew-release-bot:v0.0.46 krew-release-bot template --tag "$1" --template-file .krew.yaml > "$REPO_ROOT"/plugins/clusterctl-operator.yaml
