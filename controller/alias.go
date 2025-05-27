28fedfdec26735b0ab38d658a561c25e05215c31
Danil-Grigorev <danil.grigorev@suse.com>
2025-05-27 22:54:55 +0200

    Add internal controllers to public controller/alias.go
    This change adds existing internal controller implementations into the `controller/alias.go` file.
    
    This is an initial step towards addressing issue #798, which aims to provide a public API for CAPI Operator providers. The current controller implementation is internal, and this change is a step towards organizing the internal structure, potentially in preparation for exposing parts of it or providing extension points for custom providers.
    
    Signed-off-by: Danil-Grigorev <danil.grigorev@suse.com>

--- controller/alias.go
+++ controller/alias.go
@@ -14,8 +14,52 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/*
+Package controller provides aliases for internal controller types and functions
+to allow external users to interact with the core controller logic.
+*/
 package controller
 
-import providercontroller "sigs.k8s.io/cluster-api-operator/internal/controller"
+import (
+	"context"
 
-type GenericProviderReconciler = providercontroller.GenericProviderReconciler
+	"k8s.io/client-go/rest"
+	internalcontroller "sigs.k8s.io/cluster-api-operator/internal/controller"
+	"sigs.k8s.io/cluster-api-operator/internal/controller/genericprovider"
+	internalhealthcheck "sigs.k8s.io/cluster-api-operator/internal/controller/healthcheck"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+)
+
+// GenericProviderReconciler wraps the internal GenericProviderReconciler.
+type GenericProviderReconciler struct {
+	Provider                 genericprovider.GenericProvider
+	ProviderList             genericprovider.GenericProviderList
+	Client                   client.Client
+	Config                   *rest.Config
+	WatchConfigSecretChanges bool
+}
+
+// SetupWithManager sets up the GenericProviderReconciler with the Manager.
+func (r *GenericProviderReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager, options controller.Options) error {
+	return (&internalcontroller.GenericProviderReconciler{
+		Provider:                 r.Provider,
+		ProviderList:             r.ProviderList,
+		Client:                   r.Client,
+		Config:                   r.Config,
+		WatchConfigSecretChanges: r.WatchConfigSecretChanges,
+	}).SetupWithManager(ctx, mgr, options)
+}
+
+// ProviderHealthCheckReconciler wraps the internal ProviderHealthCheckReconciler.
+type ProviderHealthCheckReconciler struct {
+	Client client.Client
+}
+
+// SetupWithManager sets up the health check controllers with the Manager.
+func (r *ProviderHealthCheckReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager, options controller.Options) error {
+	return (&internalhealthcheck.ProviderHealthCheckReconciler{
+		Client: r.Client,
+	}).SetupWithManager(mgr, options)
+}
