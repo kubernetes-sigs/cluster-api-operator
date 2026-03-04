2f8a915951d3ce86d7db7c60f0b187c5ca4215a1
alexander-demicev <alexandr.demicev@suse.com>
2026-03-04 17:04:27 +0100

    Split phases.go into smaller files
    Signed-off-by: alexander-demicev <alexandr.demicev@suse.com>

---
+++ internal/controller/phase_fetch.go
@@ -0,0 +1,193 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	apijson "k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/client-go/kubernetes/scheme"
+	operatorv1 "sigs.k8s.io/cluster-api-operator/api/v1alpha2"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/client/repository"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/client/yamlprocessor"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Fetch fetches the provider components from the repository and processes all yaml manifests.
+func (p *PhaseReconciler) Fetch(ctx context.Context) (*Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	log.Info("Fetching provider")
+
+	// Fetch the provider components yaml file from the provided repository GitHub/GitLab/ConfigMap.
+	componentsFile, err := p.repo.GetFile(ctx, p.options.Version, p.repo.ComponentsPath())
+	if err != nil {
+		err = fmt.Errorf("failed to read %q from provider's repository %q: %w", p.repo.ComponentsPath(), p.providerConfig.ManifestLabel(), err)
+
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsFetchErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	// Check if components exceed the resource size.
+	p.needsCompression = needToCompress(componentsFile)
+
+	// Generate a set of new objects using the clusterctl library. NewComponents() will do the yaml processing,
+	// like ensure all the provider components are in proper namespace, replace variables, etc. See the clusterctl
+	// documentation for more details.
+	p.components, err = repository.NewComponents(repository.ComponentsInput{
+		Provider:     p.providerConfig,
+		ConfigClient: p.configClient,
+		Processor:    yamlprocessor.NewSimpleProcessor(),
+		RawYaml:      componentsFile,
+		Options:      p.options,
+	})
+	if err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsFetchErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	// ProviderSpec provides fields for customizing the provider deployment options.
+	// We can use clusterctl library to apply this customizations.
+	if err := repository.AlterComponents(p.components, customizeObjectsFn(p.provider)); err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	// Apply patches to the provider components if specified.
+	if err := repository.AlterComponents(p.components, applyPatches(ctx, p.provider)); err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsPatchErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	// Apply image overrides to the provider manifests.
+	if err := repository.AlterComponents(p.components, imageOverrides(p.components.ManifestLabel(), p.overridesClient)); err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsImageOverrideErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	for _, fn := range p.customAlterComponentsFuncs {
+		if err := repository.AlterComponents(p.components, fn); err != nil {
+			return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+		}
+	}
+
+	return &Result{}, nil
+}
+
+// Store stores the provider components in the cache.
+func (p *PhaseReconciler) Store(ctx context.Context) (*Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	log.Info("Storing provider in cache")
+
+	kinds, _, err := scheme.Scheme.ObjectKinds(&corev1.Secret{})
+	if err != nil || len(kinds) == 0 {
+		log.Error(err, "cannot fetch kind of the Secret resource")
+		err = fmt.Errorf("cannot fetch kind of the Secret resource: %w", err)
+
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	secret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       kinds[0].Kind,
+			APIVersion: kinds[0].GroupVersion().String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        ProviderCacheName(p.provider),
+			Namespace:   p.provider.GetNamespace(),
+			Annotations: map[string]string{},
+		},
+		StringData: map[string]string{},
+		Data:       map[string][]byte{},
+	}
+
+	gvk := p.provider.GetObjectKind().GroupVersionKind()
+
+	secret.SetOwnerReferences([]metav1.OwnerReference{
+		{
+			APIVersion: gvk.GroupVersion().String(),
+			Kind:       gvk.Kind,
+			Name:       p.provider.GetName(),
+			UID:        p.provider.GetUID(),
+		},
+	})
+
+	if p.needsCompression {
+		secret.Annotations[operatorv1.CompressedAnnotation] = "true"
+	}
+
+	manifests, err := apijson.Marshal(addNamespaceIfMissing(p.components.Objs(), p.provider.GetNamespace()))
+	if err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	if p.needsCompression {
+		var buf bytes.Buffer
+		if err := compressData(&buf, manifests); err != nil {
+			return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+		}
+
+		secret.Data["cache"] = buf.Bytes()
+	} else {
+		secret.StringData["cache"] = string(manifests)
+	}
+
+	if err := p.ctrlClient.Patch(ctx, secret, client.Apply, client.ForceOwnership, client.FieldOwner(cacheOwner)); err != nil {
+		log.Error(err, "failed to apply cache config map")
+
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsCustomizationErrorReason, operatorv1.ProviderInstalledCondition)
+	}
+
+	return &Result{}, nil
+}
+
+// addNamespaceIfMissing adda a Namespace object if missing (this ensure the targetNamespace will be created).
+func addNamespaceIfMissing(objs []unstructured.Unstructured, targetNamespace string) []unstructured.Unstructured {
+	namespaceObjectFound := false
+
+	for _, o := range objs {
+		// if the object has Kind Namespace, fix the namespace name
+		if o.GetKind() == namespaceKind {
+			namespaceObjectFound = true
+		}
+	}
+
+	// if there isn't an object with Kind Namespace, add it
+	if !namespaceObjectFound {
+		objs = append(objs, unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"apiVersion": "v1",
+				"kind":       namespaceKind,
+				"metadata": map[string]interface{}{
+					"name": targetNamespace,
+				},
+			},
+		})
+	}
+
+	return objs
+}
+
+func (p *PhaseReconciler) ReportStatus(_ context.Context) (*Result, error) {
+	status := p.provider.GetStatus()
+	status.Contract = &p.contract
+	installedVersion := p.components.Version()
+	status.InstalledVersion = &installedVersion
+	p.provider.SetStatus(status)
+
+	return &Result{}, nil
+}
---
+++ internal/controller/phase_lifecycle.go
@@ -0,0 +1,204 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	operatorv1 "sigs.k8s.io/cluster-api-operator/api/v1alpha2"
+	"sigs.k8s.io/cluster-api-operator/util"
+	clusterctlv1 "sigs.k8s.io/cluster-api/cmd/clusterctl/api/v1alpha3"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/client/cluster"
+	configclient "sigs.k8s.io/cluster-api/cmd/clusterctl/client/config"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/client/repository"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Upgrade ensure all the clusterctl CRDs are available before installing the provider,
+// and update existing components if required.
+func (p *PhaseReconciler) Upgrade(ctx context.Context) (*Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+
+	// Nothing to do if it's a fresh installation.
+	if p.provider.GetStatus().InstalledVersion == nil {
+		return &Result{}, nil
+	}
+
+	// Provider needs to be re-installed
+	if *p.provider.GetStatus().InstalledVersion == p.provider.GetSpec().Version {
+		return &Result{}, nil
+	}
+
+	log.Info("Version changes detected, updating existing components")
+
+	provider := p.providerConverter(p.provider)
+	if provider.Version == "" {
+		provider.Version = p.options.Version
+	}
+
+	if err := p.newClusterClient().ProviderUpgrader().ApplyCustomPlan(ctx, cluster.UpgradeOptions{}, cluster.UpgradeItem{
+		NextVersion: p.provider.GetSpec().Version,
+		Provider:    provider,
+	}); err != nil {
+		return &Result{}, wrapPhaseError(err, operatorv1.ComponentsUpgradeErrorReason, operatorv1.ProviderUpgradedCondition)
+	}
+
+	log.Info("Provider successfully upgraded")
+	conditions.Set(p.provider, metav1.Condition{
+		Type:    operatorv1.ProviderUpgradedCondition,
+		Status:  metav1.ConditionTrue,
+		Reason:  "ProviderUpgraded",
+		Message: "Provider upgraded successfully",
+	})
+
+	return &Result{}, nil
+}
+
+// Install installs the provider components using clusterctl library.
+func (p *PhaseReconciler) Install(ctx context.Context) (*Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+
+	// Provider was upgraded, nothing to do
+	if p.provider.GetStatus().InstalledVersion != nil && *p.provider.GetStatus().InstalledVersion != p.provider.GetSpec().Version {
+		return &Result{}, nil
+	}
+
+	clusterClient := p.newClusterClient()
+
+	log.Info("Installing provider")
+
+	if err := clusterClient.ProviderComponents().Create(ctx, p.components.Objs()); err != nil {
+		reason := "InstallFailed"
+		if wait.Interrupted(err) {
+			reason = "TimedOutWaitingForDeployment"
+		}
+
+		return &Result{}, wrapPhaseError(err, reason, operatorv1.ProviderInstalledCondition)
+	}
+
+	log.Info("Provider successfully installed")
+	conditions.Set(p.provider, metav1.Condition{
+		Type:    operatorv1.ProviderInstalledCondition,
+		Status:  metav1.ConditionTrue,
+		Reason:  "ProviderInstalled",
+		Message: "Provider installed successfully",
+	})
+
+	return &Result{}, nil
+}
+
+func convertProvider(provider operatorv1.GenericProvider) clusterctlv1.Provider {
+	clusterctlProvider := &clusterctlv1.Provider{}
+	clusterctlProvider.Name = clusterctlProviderName(provider).Name
+	clusterctlProvider.Namespace = provider.GetNamespace()
+	clusterctlProvider.Type = string(util.ClusterctlProviderType(provider))
+	clusterctlProvider.ProviderName = provider.ProviderName()
+
+	if provider.GetStatus().InstalledVersion != nil {
+		clusterctlProvider.Version = *provider.GetStatus().InstalledVersion
+	}
+
+	return *clusterctlProvider
+}
+
+// Delete deletes the provider components using clusterctl library.
+func (p *PhaseReconciler) Delete(ctx context.Context) (*Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	log.Info("Deleting provider")
+
+	clusterClient := p.newClusterClient()
+
+	provider := p.providerConverter(p.provider)
+	if provider.Version == "" {
+		provider.Version = p.options.Version
+	}
+
+	err := clusterClient.ProviderComponents().Delete(ctx, cluster.DeleteOptions{
+		Provider:         provider,
+		IncludeNamespace: false,
+		IncludeCRDs:      false,
+	})
+
+	return &Result{}, wrapPhaseError(err, operatorv1.OldComponentsDeletionErrorReason, operatorv1.ProviderInstalledCondition)
+}
+
+func clusterctlProviderName(provider operatorv1.GenericProvider) client.ObjectKey {
+	prefix := ""
+	switch provider.(type) {
+	case *operatorv1.BootstrapProvider:
+		prefix = "bootstrap-"
+	case *operatorv1.ControlPlaneProvider:
+		prefix = "control-plane-"
+	case *operatorv1.InfrastructureProvider:
+		prefix = "infrastructure-"
+	case *operatorv1.AddonProvider:
+		prefix = "addon-"
+	case *operatorv1.IPAMProvider:
+		prefix = "ipam-"
+	case *operatorv1.RuntimeExtensionProvider:
+		prefix = "runtime-extension-"
+	}
+
+	return client.ObjectKey{Name: prefix + provider.ProviderName(), Namespace: provider.GetNamespace()}
+}
+
+func (p *PhaseReconciler) repositoryProxy(ctx context.Context, provider configclient.Provider, configClient configclient.Client, options ...repository.Option) (repository.Client, error) {
+	injectRepo := p.repo
+
+	if !provider.SameAs(p.providerConfig) {
+		genericProvider, err := p.providerMapper(ctx, provider)
+		if err != nil {
+			return nil, wrapPhaseError(err, "unable to find generic provider for configclient "+string(provider.Type())+": "+provider.Name(), operatorv1.ProviderUpgradedCondition)
+		}
+
+		if exists, err := p.checkConfigMapExists(ctx, *providerLabelSelector(genericProvider), genericProvider.GetNamespace()); err != nil {
+			provider := client.ObjectKeyFromObject(genericProvider)
+			return nil, wrapPhaseError(err, "failed to check the config map repository existence for provider "+provider.String(), operatorv1.ProviderUpgradedCondition)
+		} else if !exists {
+			provider := client.ObjectKeyFromObject(genericProvider)
+			return nil, wrapPhaseError(fmt.Errorf("config map not found"), "config map repository required for validation does not exist yet for provider "+provider.String(), operatorv1.ProviderUpgradedCondition)
+		}
+
+		repo, err := p.configmapRepository(ctx, providerLabelSelector(genericProvider), InNamespace(genericProvider.GetNamespace()), SkipComponents{})
+		if err != nil {
+			provider := client.ObjectKeyFromObject(genericProvider)
+			return nil, wrapPhaseError(err, "failed to load the repository for provider "+provider.String(), operatorv1.ProviderUpgradedCondition)
+		}
+
+		injectRepo = repo
+	}
+
+	cl, err := repository.New(ctx, provider, configClient, append([]repository.Option{repository.InjectRepository(injectRepo)}, options...)...)
+	if err != nil {
+		return nil, err
+	}
+
+	return repositoryProxy{Client: cl, components: p.components}, nil
+}
+
+// newClusterClient returns a clusterctl client for interacting with management cluster.
+func (p *PhaseReconciler) newClusterClient() cluster.Client {
+	return cluster.New(cluster.Kubeconfig{}, p.configClient, cluster.InjectProxy(&controllerProxy{
+		ctrlClient: clientProxy{p.ctrlClient, p.providerLister},
+		ctrlConfig: p.ctrlConfig,
+	}), cluster.InjectRepositoryFactory(p.repositoryProxy))
+}
